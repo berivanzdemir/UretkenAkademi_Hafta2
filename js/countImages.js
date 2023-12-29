45a6bb720c66cd82876f957475c3468e94c64ab4
Berivan Özdemir <154601087+berivanzdemir@users.noreply.github.com>
2023-12-29 14:56:24 +0300

    UretkenAkademi_Hafta2

---
+++ js/countImages.js
@@ -0,0 +1,6 @@
+
+for (let i=0; i<5; i++ ){
+    const imgElement=document.createElement('img');
+    imgElement.src='https://source.unsplash.com/random/400x400';
+    document.body.appendChild(imgElement);
+}
