30097eccd3a0798ed15cc270dbe5876668f0bf8b
Berivan Özdemir <154601087+berivanzdemir@users.noreply.github.com>
2023-12-29 15:44:28 +0300

    ÜretkenAkademi_Hafta2

--- js/countImages.js
+++ js/countImages.js
@@ -1,6 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const imageGrid = document.getElementById("imageGrid");
 
-for (let i=0; i<5; i++ ){
-    const imgElement=document.createElement('img');
-    imgElement.src='https://source.unsplash.com/random/400x400';
-    document.body.appendChild(imgElement);
-}
+    for (let row = 1; row <= 5; row++) {
+        for (let col = 1; col <= 3; col++) {
+            const image = document.createElement("img");
+            image.src = `https://source.unsplash.com/random/150x150?sig=${row}${col}`;
+            image.classList.add("img");
+            imageGrid.appendChild(image);
+        }
+    }
+});
+
